9ca15b9eb32a0e66584cfbb5cf689202b0aa3417
dehlix <deborahligorio@gmail.com>
2018-10-15 00:33:21 +0200

    Update exerciseRoomBooking.js

--- exerciseRoomBooking.js
+++ exerciseRoomBooking.js
@@ -1,43 +1,77 @@
 //Room Booking // exercise for WTMB: JavaScript Crash Course Vol.2 Lecture 1
 
 
+
+
+var Guests = class {
+    constructor(guestsName){
+        this.guests=guestsName
+        this.accomodations = []
+        console.log ("We have new guests:", guestsName)
+    }
+
+    booked(Accomodation) {
+       this.accomodations.push(Accomodation)
+       Accomodation.booking.push(this)
+
+    }
+ 
+}
+
+
+
+
 var Accomodation = class {
-    constructor(roomType){
-        this.roomType=roomType
-        this.guest = []
-        console.log("type of accomodation available: " + roomType)
+    constructor(name, price, guests){
+        this.name=name
+        this.price=price
+        this.guests = []
+        this.booking = []
+        console.log("type of accomodation available: " + name, price)
     }
-    addGuest(guest){
-        this.guest.push(guest)
-        console.log("New Guest Request for", this.roomType, 'by', guest.name)
+  addGuests(guests){
+        this.guests.push(guests)
+        console.log("New Guest Request for", this.name, 'by', guests.name)
 
-    }
-       bookTheRoom(booking){
-        this.booking.push(booking)
-    }
+}
 }
 
 
 
-var Guest = class {
-    constructor(guestName){
-        console.log ("We have new guests:", guestName)
-    }
 
-}
+var lia = new Guests("Lia")
+singleRoom.addGuests(lia)
 
+var susy = new Guests("Susy")
+singleRoom.addGuests(susy)
 
-
-var doubleRoom = new Accomodation("Double Room")
-
-var singleRoom = new Accomodation("Single Room")
+var juno = new Guests ("Juno")
+doubleRoom.addGuests(juno)
 
 
-var guest1 = new Guest("Gina")
-doubleRoom.addGuest(guest1)
 
-var guest2 = new Guest("Susy")
-singleRoom.addGuest(guest2)
 
-var guest3 = new Guest("Mario and Marta")
-doubleRoom.addGuest(guest3)
+var loft = new Accomodation("Loft", 500)
+
+var doubleRoom = new Accomodation("Double Room", 200)
+
+var singleRoom = new Accomodation("Single Room", 100)
+
+var cabin = new Accomodation("Cabin", 30)
+
+
+
+
+
+
+  lia.booked(cabin)
+  console.log(lia)
+
+
+  susy.booked(doubleRoom)
+  console.log(susy)
+
+
+  juno.booked(loft)
+  console.log(juno)
+ 
